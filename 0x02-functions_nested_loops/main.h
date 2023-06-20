980dd3d5dfab1c68ccb6dc47e8495ef5dc84a40c
Mehdi Id-boussayd <idboussaydmehdi@gmail.com>
2023-06-20 15:10:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,8 +14,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n)
-void print_times_table(int n
+void print_to_98(int n);
+void print_times_table(int n);
 
-
-#endif
+#endif /* MAIN_H */
